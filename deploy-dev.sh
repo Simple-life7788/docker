545ce17266613a1fc655429b5449ffd0a1474b82
Dezhao.Zhao <956877648@qq.com>
2021-12-30 00:39:53 +0800

    ff

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -11,9 +11,9 @@ docker-compose up -d --force-recreate --build
 
 
 # 定制镜像
-docker build -t myapp:pm2 ./backend
+# docker build -t myapp:pm2 ./backend
 
 # 重启启动容器
-docker stop myapp
-docker rm myapp
-docker run --name myapp -p 3000:3000  -d myapp:pm2
+# docker stop myapp
+# docker rm myapp
+# docker run --name myapp -p 3000:3000  -d myapp:pm2
